363efb71cd29d917d570f5893145c6fe2411a294
Unknown <emilsergiev@abv.bg>
2017-03-25 13:57:19 +0200

    added the brick

--- src/brickbreaker/classes/Brick.java
+++ src/brickbreaker/classes/Brick.java
@@ -1,5 +1,39 @@
 package brickbreaker.classes;
 
-public class Brick extends GameObject {
-	//TODO the Brick class...
+import javax.swing.ImageIcon;
+
+public class Brick extends GameObject
+{
+	private boolean powerup;
+
+	protected Brick(int x, int y, String img)
+	{
+		super();
+		this.x = x;
+		this.y = y;
+
+		setImage(img);
+
+		width = image.getWidth(null);
+		height = image.getHeight(null);
+
+		setDestroyed(false);
+		setPowerup(false);
+	}
+
+	protected void setImage(String img)
+	{
+		ImageIcon ii = new ImageIcon(getURL(img));
+		image = ii.getImage();
+	}
+
+	protected boolean isPowerup()
+	{
+		return powerup;
+	}
+
+	protected void setPowerup(boolean powerup)
+	{
+		this.powerup = powerup;
+	}
 }
