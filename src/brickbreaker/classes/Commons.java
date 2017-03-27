d85e226a47d2ff0bb72d9c9f1a6c1f683b34cdc5
Unknown <emilsergiev@abv.bg>
2017-03-27 15:22:19 +0300

    added game constants

--- src/brickbreaker/classes/Commons.java
+++ src/brickbreaker/classes/Commons.java
@@ -2,7 +2,18 @@ package brickbreaker.classes;
 
 public interface Commons
 {
-	public static final int WIDTH = 500;
-	public static final int HEIGHT = 700;
-	//TODO add the common constants here...
+	final int WALL = 38;
+	final int WIDTH = 500;
+	final int HEIGHT = 700;
+	final int BOTTOM = 650;
+	final int MESSAGE_X = 180;
+	final int MESSAGE_Y = 300;
+	final int PADDLE_X = 200;
+	final int PADDLE_Y = 600;
+	final int PADDLE_SPEED = 3;
+	final int BALL_X = 230;
+	final int BALL_Y = 580;
+	final int BALL_SPEED = 2;
+	final int DELAY = 1000;
+	final int PERIOD = 10;
 }
