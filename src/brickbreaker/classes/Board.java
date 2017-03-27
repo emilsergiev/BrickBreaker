6083b949687cd1fe6801898fe6d1fb866c8de083
Unknown <emilsergiev@abv.bg>
2017-03-27 15:41:37 +0300

    did the main game logic

--- src/brickbreaker/classes/Board.java
+++ src/brickbreaker/classes/Board.java
@@ -2,11 +2,12 @@ package brickbreaker.classes;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Timer;
@@ -15,49 +16,74 @@ import javax.swing.JPanel;
 public class Board extends JPanel implements Commons
 {
 	private static final long serialVersionUID = 1L;
-	private String message = "Welcome ^.^";
-	private static Timer timer;
-	private Graphics2D g;
-	private Paddle paddle;
-	private boolean play;
-	private int score = 0;
-	private int level = 1;
 	private int ingame = 1;
-
-	Random random = new Random();
-
-	ArrayList<Ball> balls = new ArrayList<Ball>();
-	ArrayList<Brick> bricks = new ArrayList<Brick>();
-	ArrayList<Spider> spiders = new ArrayList<Spider>();
+	private int level = 1;
+	private int score = 0;
+	private boolean play;
+	private boolean paused;
+	private Timer timer;
+	private Paddle paddle;
+	private Random random = new Random();
+	private ArrayList<Ball> balls = new ArrayList<Ball>();
+	private ArrayList<Brick> bricks = new ArrayList<Brick>();
+	private ArrayList<Spider> spiders = new ArrayList<Spider>();
+	private String message = "<ENTER> to play";
+	private FreeGift gift;
 
 	protected Board()
 	{
-		initBoard();
-		initGame();
-	}
-
-	private void initBoard()
-	{
-		//TODO init stuff...
-
-		//timer = new Timer();
-		//timer.scheduleAtFixedRate(new Animate(this), 1000, 10);
-	}
-
-	private void initGame()
-	{
 		setBricks();
-		//TODO set the ball and paddle
+		setFocusable(true);
+		setDoubleBuffered(true);
+		addKeyListener(new MyAdapter());
+	}
+
+	protected void playGame()
+	{
+		if (!play)
+		{
+			timer = new Timer();
+			timer.scheduleAtFixedRate(new Animate(this), DELAY, PERIOD);
+			paddle = new Paddle();
+			balls.add(new Ball());
+			play = true;
+		}
 	}
 
 	private void setBricks()
 	{
-		//TODO set the bricks...
-	}
+		for (int i = 0; i < 8; i++)
+		{
+			bricks.add(new Brick((i * 60 + 7), 50, "/brickbreaker/resources/brick.png"));
+		}
+		for (int i = 0; i < 7; i++)
+		{
+			bricks.add(new Brick((i * 60 + 37), 75, "/brickbreaker/resources/brick.png"));
+		}
+		for (int i = 0; i < 8; i++)
+		{
+			bricks.add(new Brick((i * 60 + 7), 100, "/brickbreaker/resources/brick.png"));
+		}
+		for (int i = 0; i < 7; i++)
+		{
+			bricks.add(new Brick((i * 60 + 37), 125, "/brickbreaker/resources/brick.png"));
+		}
+		for (int i = 0; i < 8; i++)
+		{
+			bricks.add(new Brick((i * 60 + 7), 150, "/brickbreaker/resources/brick.png"));
+		}
+		for (int i = 0; i < 6; i++)
+		{
+			bricks.get(random.nextInt(WALL)).setPowerup(true);
+		}
 
-	protected void update()
-	{
-		//TODO the game logic here...
+		for (Brick b : bricks)
+		{
+			if (b.isPowerup())
+			{
+				b.setImage("/brickbreaker/resources/brick-spider.png");
+			}
+		}
 	}
 
 	@Override
@@ -65,42 +91,223 @@ public class Board extends JPanel implements Commons
 	{
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
-
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setColor(Color.BLACK);
+		g2d.fillRect(0, 0, Commons.WIDTH, Commons.HEIGHT);
 
-		//TODO draw objects...
-
-//		drawStart(g2d);
-
-		displayMessage(g2d);
-		
+		if (play)
+		{
+			drawObjects(g2d);
+		}
+		else
+		{
+			displayMessage(g2d);
+		}
 		g2d.dispose();
 		Toolkit.getDefaultToolkit().sync();
-	}
-	
-	public void drawStart(Graphics2D g2d)
-	{
-		for (int i = 100; i > 0; i--)
-		{
-			g2d.setColor(Color.BLACK);
-			g2d.fillRect(0, 0, Commons.WIDTH, Commons.HEIGHT);
-			g2d.setColor(Color.WHITE);
-			g2d.setFont(new Font("Courier New", Font.BOLD, i));
-			g2d.drawString("Start!", Commons.WIDTH / 2 - i * 2 + 20, Commons.HEIGHT / 2);
-			repaint();
-			try { Thread.sleep(20); }
-			catch (Exception e) { e.printStackTrace(); }
-		}
 	}
 
 	private void displayMessage(Graphics2D g2d)
 	{
-		Font font = new Font("Verdana", Font.BOLD, 18);
-		FontMetrics metr = this.getFontMetrics(font);
 		g2d.setColor(Color.RED);
-		g2d.setFont(font);
-		g2d.drawString(message, (Commons.WIDTH - metr.stringWidth(message)) / 2, Commons.WIDTH / 2);
+		g2d.setFont(new Font("Verdana", Font.BOLD, 17));
+		g2d.drawString(message, MESSAGE_X, MESSAGE_Y);
+	}
+
+	private void drawObjects(Graphics2D g2d)
+	{
+		g2d.setColor(Color.WHITE);
+		g2d.setFont(new Font("Verdana", Font.BOLD, 17));
+		g2d.drawString("Level: " + level, 5, 20);
+		g2d.drawString("Score: " + score, 350, 20);
+
+		g2d.drawImage(paddle.image, paddle.x, paddle.y, paddle.width, paddle.height, this);
+
+		if (gift != null)
+		{
+			g2d.drawImage(gift.image, gift.x, gift.y, gift.width, gift.height, this);
+		}
+
+		for (Brick brick : bricks)
+		{
+			if (!brick.isDestroyed())
+			{
+				g2d.drawImage(brick.image, brick.x, brick.y, brick.width, brick.height, this);
+			}
+		}
+		for (Ball ball : balls)
+		{
+			if (!ball.isDestroyed())
+			{
+				ball.move();
+				g2d.drawImage(ball.image, ball.x, ball.y, ball.width, ball.height, this);
+			}
+		}
+		for (Spider spider : spiders)
+		{
+			if (!spider.isDestroyed())
+			{
+				spider.move();
+				g2d.drawImage(spider.image, spider.x, spider.y, spider.width, spider.height, this);
+			}
+		}
+	}
+
+	protected void update()
+	{
+		if (ingame <= 0)
+		{
+			endGame();
+		}
+		if (gift != null){
+			gift.move();
+			catchTheGift();
+		}
+		paddle.move();
+		checkBrickCount();
+		catchTheSpiders();
+		checkBallCollisions();
+	}
+
+	private void catchTheGift()
+	{
+		if (gift.getRect().intersects(paddle.getRect()))
+		{
+			gift = null;
+			for (Ball ball : balls) {
+				if (ball.getSpeed() >= 3)
+				{
+					ball.setSpeed(BALL_SPEED);
+				}
+				else if (paddle.width <= 50)
+				{
+					paddle.setWidth(75);
+				}
+				else
+				{
+					score += 10;
+				}
+			}
+		}
+	}
+
+	private void checkBrickCount()
+	{
+		int brickCount = 0;
+		for (Brick brk : bricks)
+		{
+			if (brk.isDestroyed())
+			{
+				brickCount++;
+			}
+			if (brickCount == WALL)
+			{
+				score += 10;
+				level++;
+				bricks.clear();
+				setBricks();
+				giveFreeGift();
+			}
+		}
+	}
+
+	private void giveFreeGift() {
+		if (gift == null)
+		{
+			gift = new FreeGift();
+		}
+		else
+		{
+			score += 20;
+		}
+	}
+
+	private void catchTheSpiders()
+	{
+		for (Spider spider : spiders)
+		{
+			if (spider.getRect().intersects(paddle.getRect()) && !spider.isDestroyed())
+			{
+				spider.setDestroyed(true);
+				if (spider.isExtra())
+				{
+					balls.add(new Ball(spider.getX(), spider.getY()));
+				}
+				else if (paddle.width < 125)
+				{
+					paddle.setWidth(paddle.width + 25);
+					ingame--;
+				}
+				else
+				{
+					balls.add(new Ball(spider.getX(), spider.getY()));
+				}
+			}
+			if (spider.getRect().getMaxY() > BOTTOM && !spider.isDestroyed())
+			{
+				ingame--;
+				spider.setDestroyed(true);
+				if (paddle.width > 25){
+					paddle.setWidth(paddle.width - 25);
+				}
+			}
+		}
+	}
+
+	private void checkBallCollisions()
+	{
+		for (Ball ball : balls)
+		{
+			if (ball.getRect().getMaxY() > BOTTOM && !ball.isDestroyed())
+			{
+				ball.setDestroyed(true);
+				ingame--;
+				if (paddle.width > 25){
+					paddle.setWidth(paddle.width - 25);
+				}
+			}
+			if (ball.getRect().intersects(paddle.getRect()))
+			{
+				ball.setYdir(ball.getYdir() * -1);
+			}
+			for (Brick brick : bricks)
+			{
+				if ((brick.getLeft().intersects(ball.getRect()) || brick.getRight().intersects(ball.getRect()))
+						&& !brick.isDestroyed())
+				{
+					ball.setXdir(ball.getXdir() * -1);
+					score++;
+					if (brick.isPowerup())
+					{
+						brick.setImage("/brickbreaker/resources/brick.png");
+						brick.setPowerup(false);
+						spiders.add(new Spider(brick.x, brick.y, getRandomBoolean()));
+						ingame++;
+					}
+					else
+					{
+						brick.setDestroyed(true);
+					}
+				}
+				else if (ball.getRect().intersects(brick.getRect()) && !brick.isDestroyed())
+				{
+					ball.setYdir(ball.getYdir() * -1);
+					score++;
+					if (brick.isPowerup())
+					{
+						brick.setImage("/brickbreaker/resources/brick.png");
+						brick.setPowerup(false);
+						spiders.add(new Spider(brick.x, brick.y, getRandomBoolean()));
+						ingame++;
+					}
+					else
+					{
+						brick.setDestroyed(true);
+					}
+				}
+			}
+		}
 	}
 
 	private boolean getRandomBoolean()
@@ -108,8 +315,58 @@ public class Board extends JPanel implements Commons
 		return random.nextBoolean();
 	}
 
-	protected void setPlay(boolean p)
+	private class MyAdapter extends KeyAdapter
 	{
-		play = p;
+		@Override
+		public void keyReleased(KeyEvent e)
+		{
+			if (play)
+			{
+				paddle.keyReleased(e);
+			}
+		}
+		@Override
+		public void keyPressed(KeyEvent e)
+		{
+			if (play)
+			{
+				paddle.keyPressed(e);
+			}
+
+			if (e.getKeyCode() == KeyEvent.VK_SPACE)
+			{
+				if (!paused) { pause(); }
+				else { resume(); }
+			}
+
+			if (e.getKeyCode() == KeyEvent.VK_ENTER)
+			{
+				playGame();
+			}
+		}
+	}
+
+	protected void pause()
+	{
+		if (play)
+		{
+			timer.cancel();
+			paused = true;
+		}
+	}
+
+	protected void resume()
+	{
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new Animate(this), DELAY, PERIOD);
+		paused = false;
+	}
+
+	private void endGame()
+	{
+		play = false;
+		message = "Game Over!";
+		timer.cancel();
+		timer.purge();
 	}
 }
