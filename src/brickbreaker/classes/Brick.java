87cab4b911af65967c868f6754b0d943303e38b1
Unknown <emilsergiev@abv.bg>
2017-03-29 12:06:32 +0300

    removed unneeded methods

--- src/brickbreaker/classes/Brick.java
+++ src/brickbreaker/classes/Brick.java
@@ -1,38 +1,24 @@
 package brickbreaker.classes;
 
-import javax.swing.ImageIcon;
-
 public class Brick extends GameObject
 {
 	private boolean powerup;
 
-	protected Brick(int x, int y, String img)
+	Brick(int x, int y, String img)
 	{
 		super();
 		this.x = x;
 		this.y = y;
-
 		setImage(img);
-
-		width = image.getWidth(null);
-		height = image.getHeight(null);
-
-		setDestroyed(false);
 		setPowerup(false);
 	}
 
-	protected void setImage(String img)
-	{
-		ImageIcon ii = new ImageIcon(getURL(img));
-		image = ii.getImage();
-	}
-
-	protected boolean isPowerup()
+	boolean isPowerup()
 	{
 		return powerup;
 	}
 
-	protected void setPowerup(boolean powerup)
+	void setPowerup(boolean powerup)
 	{
 		this.powerup = powerup;
 	}
